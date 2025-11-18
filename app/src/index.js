9893a66a5be11582ca80046a611a104b3caf3bb4
DINTDT <dintdt@gmail.com>
2025-11-18 18:14:38 -0300

    Añadido mostrar notificaciones de Android
    - El archivo mobile.js define la función showNotification(), que crea una notificación.

--- app/src/index.js
+++ app/src/index.js
@@ -1,6 +1,7 @@
 import '@capacitor/core';
 import { App } from '@capacitor/app';
 import { Device } from '@capacitor/device';
+import { LocalNotifications } from '@capacitor/local-notifications';
 
 App.addListener("backButton",(ev)=>{
 	let modal = document.querySelector(".modalBackdrop") || document.querySelector(".popUpBackdrop");
@@ -18,4 +19,19 @@ window.addEventListener("load",(ev)=>{
 		return id;
 	};
 	
+	// Solicita a Android que muestre una notificación con el título ${title} y el contenido ${body}
+	// en la fecha ${date}, en formato de string de fecha. No siempre se muestra en el tiempo exacto.
+	// new Date(Date.now() + X))  entrega la fecha de X segundos en el futuro en el formato correcto.
+	window.showNotification = async(title, body, date)=>{
+		console.log(`showNotification: {title: [${title}], body:[${body}], date:[${date}]}`)
+		await LocalNotifications.schedule({
+			notifications: [{
+				title: title,
+				body: body,
+				id: Math.ceil(Math.random() * 100), // any random int
+				schedule: { at: date },
+				sound: null
+			}]
+		});
+	}
 })
